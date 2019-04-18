a753315baf02ff4a625a6a5dac82b2b7bc7d80c9
Stella Widyasari <stellaw@thoughtworks.com>
2019-04-18 15:56:57 +0800

    [Add]deployment pipeline to kubernetes

---
+++ install-upgrade-helm.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+name=${CHART_NAME:-undefined}
+path=${CHART_PATH:-undefined}
+version=${VERSION:-""}
+values_file=${VALUES_FILE_PATH:-undefined}
+namespace=${NAMESPACE:-undefined}
+options=${OPTIONS:-""}
+
+pushd /tmp
+curl -O https://kubernetes-helm.storage.googleapis.com/helm-v2.11.0-linux-amd64.tar.gz
+tar xf helm-v2.11.0-linux-amd64.tar.gz
+export PATH=$(pwd)/linux-amd64:$PATH
+popd
+
+helm init --client-only
+helm repo add incubator http://storage.googleapis.com/kubernetes-charts-incubator
+helm repo update
+
+helm upgrade --install ${name} --values=${values_file} \
+  --version=${version} --namespace=${namespace} ${path} \
+  --set=$options
+
+
