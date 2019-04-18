6af3a502c400e33a7fab74db72574ec708ec2277
Stella Widyasari <stellaw@thoughtworks.com>
2019-04-18 16:42:29 +0800

    [Update]Static page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.write('Hello World');
+  res.write('Hello World Another One');
   res.end();
 }).listen(3000, () => {
   console.log('Server start at port 3000');
