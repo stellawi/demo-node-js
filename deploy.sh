d71b657661bd717cdfe1d67b1e428f418e47dac8
Stella Widyasari <stellaw@thoughtworks.com>
2019-04-23 15:33:14 +0800

    [Update]Deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ echo "This is deploy script"
 echo $1
 
 unzip -o $1
-ls Archive/
 
-cp Archive/index.js /home/ec2-user/nodejs/sample-app/
+cp index.js /home/ec2-user/nodejs/sample-app/
 sudo /bin/systemctl restart node_app.service
