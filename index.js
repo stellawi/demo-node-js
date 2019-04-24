0d0e1f6c3bfb3dea1e835cb752de3f7fd7e4f1c4
Stella Widyasari <stellaw@thoughtworks.com>
2019-04-24 15:20:14 +0800

    [Modify]index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.write('Hello World is coming from the sample nodejs');
+  res.write('Hello World is here');
   res.end();
 }).listen(3000, () => {
   console.log('Server start at port 3000');
